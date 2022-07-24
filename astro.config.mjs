576c4e931a6a2d1c040f6920660c096276c10c31
Mateusz Poniatowski <pinkyball50@gmail.com>
2022-07-24 18:36:53 +0200

    refactor: :chart_with_upwards_trend: Remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), partytown()]
+  integrations: [tailwind(), react()],
 });
